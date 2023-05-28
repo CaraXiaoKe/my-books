85c6e2020dcca0de77f3e3de160924412a7b7da6
杜培东 <dupeidong02@meituan.com>
2023-05-29 00:33:47 +0800

    feat: 移除log

--- packages/blog/src/pages/api/restart.ts
+++ packages/blog/src/pages/api/restart.ts
@@ -1,24 +1,11 @@
 
 import type { APIRoute } from 'astro';
-import fs from 'fs'
-// import  { exec } from 'child_process'
+import  { exec } from 'child_process'
 export const post: APIRoute =  async ({ request }) => {
-  // exec('git pull origin main && pm2 restart blog')
   const body = await request.json();
-  fs.writeFileSync( './info.log', JSON.stringify(body), {
-    encoding: 'utf-8'
-  })
-  return  new Response(JSON.stringify(true), {
-    status: 200,
-    headers: {
-      "Content-Type": "application/json"
-    }
-  });
-}
-
-
-export const get: APIRoute =  ({ request }) => {
-  // exec('git pull origin main && pm2 restart blog')
+  if(body.ref === 'refs/heads/main'){
+    exec('git pull origin main && pm2 restart blog')
+  }
   return  new Response(JSON.stringify(true), {
     status: 200,
     headers: {
