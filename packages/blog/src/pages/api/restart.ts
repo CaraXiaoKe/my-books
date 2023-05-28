5b41e15d081339729d45a239a4ebe084d53a7aa2
杜培东 <dupeidong02@meituan.com>
2023-05-29 00:08:01 +0800

    feat: 测试webhook

--- packages/blog/src/pages/api/restart.ts
+++ packages/blog/src/pages/api/restart.ts
@@ -1,12 +1,24 @@
 
 import type { APIRoute } from 'astro';
 import fs from 'fs'
-import path from 'path'
 // import  { exec } from 'child_process'
-
 export const post: APIRoute =  ({ request }) => {
   // exec('git pull origin main && pm2 restart blog')
-  fs.writeFileSync(path.join(__dirname, './info.log'), JSON.stringify(request.body), {
+  fs.writeFileSync( './info.log', JSON.stringify(request.body), {
+    encoding: 'utf-8'
+  })
+  return  new Response(JSON.stringify(true), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+}
+
+
+export const get: APIRoute =  ({ request }) => {
+  // exec('git pull origin main && pm2 restart blog')
+  fs.writeFileSync('./info.log', JSON.stringify(request), {
     encoding: 'utf-8'
   })
   return  new Response(JSON.stringify(true), {
